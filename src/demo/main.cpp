70bdb70038e1861e2e07c9e231dc73dbf284df9a
Erx <e.a.erath@gmail.com>
2025-07-16 13:13:36 +0200

    testing if it blocks PRs

--- src/demo/main.cpp
+++ src/demo/main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int main() {
   "this does not compile!"
   "am testing branchprotection on main"
+  "testing if it also blocks PRs"
   Matrix a = Matrix::Identity(3);
   Matrix b(3, 3);
   b[2, 0] = 1;
